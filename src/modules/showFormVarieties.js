a6e845f652580b698b16b04fb4bb603967ac7718
Luiz Henrique <luizhf42@gmail.com>
2022-01-30 15:23:13 -0300

    adding a div (made manually) to show the rotom varieties
    the default div shows only 3 forms and rotom has 6

--- src/modules/showFormVarieties.js
+++ src/modules/showFormVarieties.js
@@ -5,33 +5,44 @@ const defaultFormImg = document.getElementById("default-form"),
   secondVarietyImg = document.getElementById("second-variety"),
   secondVarietyName = document.getElementById("second-variety-name");
 
+const rotomVarietiesDiv = document.getElementById("rotom-varieties"),
+  varietiesDiv = document.getElementById("varieties");
+
 export const showFormVarieties = (varieties, id) => {
-  varieties.forEach((variety) => {
-    if (variety.is_default) {
-      defaultFormImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-      defaultFormName.innerText = variety.pokemon.name;
+  if (id === 479) {
+    rotomVarietiesDiv.style.display = "flex";
+    varietiesDiv.style.display = "none";
+  } else {
+    rotomVarietiesDiv.style.display = "none";
+    varietiesDiv.style.display = "flex";
+
+    varieties.forEach((variety) => {
+      if (variety.is_default) {
+        defaultFormImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+        defaultFormName.innerText = variety.pokemon.name;
+      }
+    });
+  
+    if (varieties[1]) {
+      let varietyId = varieties[1].pokemon.url;
+      varietyId = varietyId.substring(34);
+      varietyId = varietyId.replace("/", "");
+      firstVarietyImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${varietyId}.png`;
+      firstVarietyName.innerText = varieties[1].pokemon.name;
+      firstVarietyImg.parentElement.style.display = "flex";
+    } else {
+      firstVarietyImg.parentElement.style.display = "none";
     }
-  });
-
-  if (varieties[1]) {
-    let varietyId = varieties[1].pokemon.url;
-    varietyId = varietyId.substring(34);
-    varietyId = varietyId.replace("/", "");
-    firstVarietyImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${varietyId}.png`;
-    firstVarietyName.innerText = varieties[1].pokemon.name;
-    firstVarietyImg.parentElement.style.display = "flex";
-  } else {
-    firstVarietyImg.parentElement.style.display = "none";
-  }
-
-  if (varieties[2]) {
-    let varietyId = varieties[2].pokemon.url;
-    varietyId = varietyId.substring(34);
-    varietyId = varietyId.replace("/", "");
-    secondVarietyImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${varietyId}.png`;
-    secondVarietyName.innerText = varieties[2].pokemon.name;
-    secondVarietyImg.parentElement.style.display = "flex";
-  } else {
-    secondVarietyImg.parentElement.style.display = "none";
+  
+    if (varieties[2]) {
+      let varietyId = varieties[2].pokemon.url;
+      varietyId = varietyId.substring(34);
+      varietyId = varietyId.replace("/", "");
+      secondVarietyImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${varietyId}.png`;
+      secondVarietyName.innerText = varieties[2].pokemon.name;
+      secondVarietyImg.parentElement.style.display = "flex";
+    } else {
+      secondVarietyImg.parentElement.style.display = "none";
+    }
   }
 };
